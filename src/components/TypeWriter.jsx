50c151575cc657bbfa23960958c70a53461df327
Ariyawat-N <133693268+Ariyawat-N@users.noreply.github.com>
2023-10-12 02:17:17 +0700

    edit font-size Homepage

--- src/components/TypeWriter.jsx
+++ src/components/TypeWriter.jsx
@@ -3,7 +3,7 @@ import TypewriterComponent from 'typewriter-effect'
 
 const TypeWriter = () => {
   return (
-    <div className='text-white font-fontTwo text-6xl italic mt-12'>
+    <div className='text-white font-fontTwo text-5xl italic mt-12'>
       {/* text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600 */}
         <TypewriterComponent
         options={{
